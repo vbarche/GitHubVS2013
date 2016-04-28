e8be6403b60ffe2259db5900a334eaed5d7761fd
vbarche <vikramsbarche@gmail.com>
2016-04-28 06:08:24 +0530

    Update Program.cs

--- Git/Program.cs
+++ Git/Program.cs
@@ -19,6 +19,7 @@ namespace Git
             Console.WriteLine("Commit 1");
             Console.WriteLine("Commit 2");
             Console.WriteLine("Commit 3");
+            Console.WriteLine("this only reflect when pull");
         }
     }
 }

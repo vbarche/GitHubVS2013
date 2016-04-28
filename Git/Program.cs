628322b9c10960f55d7cb77fbff4282610a4cd67
vbarche <vikramsbarche@gmail.com>
2016-04-28 05:47:37 +0530

    Update Program.cs
    added comment to learn sync in local repository.

--- Git/Program.cs
+++ Git/Program.cs
@@ -11,6 +11,7 @@ namespace Git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Git");
+            Console.WriteLine("this is change by others pull request");
         }
     }
 }
